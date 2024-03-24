194df9e41d095ed995e09c60c00da4d0c17d6e4a
Sula <turatbekovchannel2018@gmail.com>
2024-03-24 17:45:28 +0600

    make section AllParsingList

--- src/routes/routesConfig.tsx
+++ src/routes/routesConfig.tsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom'
 import App from '../App'
 import {Second} from '../components/Second/Second'
-import {Home} from '../components/Home/Home'
+import {Home} from '@/src/common/Home/Home'
 import {NotFoundPage} from "../components/NotFoundPage/NotFoundPage.tsx";
 const routeConfig = createBrowserRouter([
 	{
