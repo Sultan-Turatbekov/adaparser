b46ca1e3d4e00b8d0f3b4afbcefd51e3a74675f4
UchihaAdik <adilvar2001@gmail.com>
2024-03-26 18:14:33 +0600

    header modul

--- src/common/Header/Header.tsx
+++ src/common/Header/Header.tsx
@@ -2,7 +2,8 @@ import {Link} from "react-router-dom";
 import  adaUse  from './adaUse.json';
 import  resources  from './resources.json';
 import { BurgerMenu } from "../../components/shared/burgerMenu/BurgerMenu";
-import "./style.scss"
+import styles from './Header.module.scss';
+
 
 
 
@@ -16,57 +17,55 @@ import {
   } from "@/src/components/ui/navigation-menu"
 
 
-export const Header = () => {
-    return(
-        <header  className={` bg-dark_full `}>
-            <div className="header_conteiner">
-            <NavigationMenu className="NavigationMenu">
-            <div className="header__logoConteiner">
-                <Link to="/">
-                <img src="logo.png" alt="logo" />
-                </Link>
-            </div>
-            <NavigationMenuList className="NavigationMenuItem">
-                <NavigationMenuItem >
-                <NavigationMenuTrigger className="NavigationMenuTrigger">Кто использует ADA-Parser</NavigationMenuTrigger>
-                <NavigationMenuContent className="NavigationMenuContent">
-                {adaUse.map((item) => (
-                <Link to="#" key={item.subtitle}>
-                    <div className="Header__link">
-                        <img className="Header__link_img" src={item.img} alt="" />
-                        <NavigationMenuLink className="Header__link_subtitle">{item.subtitle}</NavigationMenuLink>
-                        <p>{item.text}</p>
+  export const Header = () => {
+    return (
+        <header className={`${styles.full_bleed }`}>
+            <div className={styles.header_container}>
+                <NavigationMenu className={styles.NavigationMenu}>
+                    <div className={styles.header_logoContainer}>
+                        <Link to="/">
+                            <img src="logoWi.svg" alt="logo" />
+                        </Link>
                     </div>
-                </Link>
-                ))}
- 
-                </NavigationMenuContent>
-                </NavigationMenuItem>
+                    <NavigationMenuList className={styles.NavigationMenuItem}>
+                        <NavigationMenuItem >
+                            <NavigationMenuTrigger className={styles.NavigationMenuTrigger}>Кто использует ADA-Parser</NavigationMenuTrigger>
+                            <NavigationMenuContent className={styles.NavigationMenuContent}>
+                                {adaUse.map((item) => (
+                                    <Link to="#" key={item.subtitle}>
+                                        <div className={styles.Header_link}>
+                                            <img className={styles.Header_link_img} src={item.img} alt="" />
+                                            <NavigationMenuLink className={styles.Header_link_subtitle}>{item.subtitle}</NavigationMenuLink>
+                                            <p>{item.text}</p>
+                                        </div>
+                                    </Link>
+                                ))}
+                            </NavigationMenuContent>
+                        </NavigationMenuItem>
 
-                <NavigationMenuItem>
-                <NavigationMenuTrigger className="NavigationMenuTrigger">Ресурсы</NavigationMenuTrigger>
-                <NavigationMenuContent className="NavigationMenuContent2">
-                {resources.map((item) => (
-                <div key={item.subtitle} className="Header__link">
-                    <img className="Header__link_img" src={item.img} alt="" />
-                    <NavigationMenuLink className="Header__link_subtitle">{item.subtitle}</NavigationMenuLink>
-                    <p>{item.text}</p>
+                        <NavigationMenuItem>
+                            <NavigationMenuTrigger className={styles.NavigationMenuTrigger}>Ресурсы</NavigationMenuTrigger>
+                            <NavigationMenuContent className={styles.NavigationMenuContent2}>
+                                {resources.map((item) => (
+                                    <div key={item.subtitle} className={styles.Header_link}>
+                                        <img className={styles.Header_link_img} src={item.img} alt="" />
+                                        <NavigationMenuLink className={styles.Header_link_subtitle}>{item.subtitle}</NavigationMenuLink>
+                                        <p>{item.text}</p>
+                                    </div>
+                                ))}
+                            </NavigationMenuContent>
+                        </NavigationMenuItem>
+                    </NavigationMenuList>
+                    <Link to="/parsings-list"><h2 className={`${styles.NavigationMenuTrigger} ${styles.NavigationMenuTrigg}`}>Парсеры</h2></Link>
+                    <Link to="/servicesPage"><h2 className={styles.NavigationMenuTrigge}>Услуги</h2></Link>
+                    <Link to="#"><h2 className={styles.NavigationMenuTrigge}>Форум</h2></Link>
+                </NavigationMenu>
+                <Link to="#"><button className={styles.header_button}>Купить ADA-Parser</button></Link>
+                <BurgerMenu />
+                <div className={styles.header_logo_media}>
+                    <img src="logoWi.svg" alt="" />
                 </div>
-                ))}   
-                </NavigationMenuContent>
-                </NavigationMenuItem>
-            </NavigationMenuList>
-            <Link to="/parsings-list"><h2 className="NavigationMenuTrigge NavigationMenuTrigg">Парсеры</h2></Link>
-            <Link to="/servicesPage"><h2 className="NavigationMenuTrigge">Услуги</h2></Link>
-            <Link to="#"><h2 className="NavigationMenuTrigge">Форум</h2></Link>
-            </NavigationMenu>
-            <Link to="#"><button className="header_button">Купить ADA-Parser</button></Link>
-           <BurgerMenu/>
-            <div className="header_logo_media">
-                <img src="logo.png" alt="" />
             </div>
-            </div>
-            
         </header>
-    )
+    );
 }
--- src/components/shared/burgerMenu/BurgerMenu.tsx
+++ src/components/shared/burgerMenu/BurgerMenu.tsx
@@ -1,7 +1,7 @@
 import {Link} from "react-router-dom";
 import   adaUse   from '../../../common/Header/adaUse.json';
 import  resources  from '../../../common/Header/resources.json';
-import "./style.scss"
+import styles from './BurgerMenu.module.scss';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
@@ -25,54 +25,52 @@ import {
 
 export const BurgerMenu = () => {
     return(
-        <div className="BurgerMenu">
+        <div className={styles.BurgerMenu}>
             <Sheet>
-            <SheetTrigger><FontAwesomeIcon id="BurgerMenu_icon" icon={faBars} /></SheetTrigger>
-            <SheetContent>
-                <SheetHeader>
-                <SheetTitle>ADA-Parser</SheetTitle>
-                <SheetDescription>
-                <NavigationMenu className="BurgerMenu_NavigationMenu">
-            <NavigationMenuList className="NavigationMenuList">
-                <NavigationMenuItem >
-                <NavigationMenuTrigger className="NavigationMenuTrigger">Кто использует ADA-Parser</NavigationMenuTrigger>
-                <NavigationMenuContent className="NavigationMenuContent">
-                    {adaUse.map((item)=>(
-                        <Link to="#">
-                            <div key={item.subtitle} className="Header__link">
-                            <img className="Header__link_img" src={item.img} alt="" />
-                            <NavigationMenuLink className="Header__link_subtitle">{item.subtitle}</NavigationMenuLink>
-                           
-                        </div>
-                        </Link>
-                        
-                    ))}      
-                </NavigationMenuContent>
-                </NavigationMenuItem>
-
-                <NavigationMenuItem>
-                <NavigationMenuTrigger className="NavigationMenuTrigger resurs">Ресурсы</NavigationMenuTrigger>
-                <NavigationMenuContent className="NavigationMenuContent2">
-                    {resources.map((item)=>(
-                        <div key={item.subtitle} className="Header__link">
-                            <img className="Header__link_img" src={item.img} alt="" />
-                            <NavigationMenuLink className="Header__link_subtitle">{item.subtitle}</NavigationMenuLink>
-                            <p>{item.text}</p>
-                        </div>
-                    ))}      
-                </NavigationMenuContent>
-                </NavigationMenuItem>
-            </NavigationMenuList>
-            <Link to="/parsings-list"><h2 className="NavigationMenuTrigge NavigationMenuTrigg">Парсеры</h2></Link>
-            <Link to="/servicesPage"><h2 className="NavigationMenuTrigge">Услуги</h2></Link>
-            <Link to="#"><h2 className="NavigationMenuTrigge">Форум</h2></Link>
-            </NavigationMenu>
-            <Link to="#"><button className="BurgerMenu_button">Купить ADA-Parser</button></Link>
-                </SheetDescription>
-                </SheetHeader>
-            </SheetContent>
+                <SheetTrigger>
+                    <FontAwesomeIcon id={styles.BurgerMenu_icon} icon={faBars} />
+                </SheetTrigger>
+                <SheetContent>
+                    <SheetHeader>
+                        <SheetTitle>ADA-Parser</SheetTitle>
+                        <SheetDescription>
+                            <NavigationMenu className={styles.BurgerMenu_NavigationMenu}>
+                                <NavigationMenuList className={styles.NavigationMenuList}>
+                                    <NavigationMenuItem>
+                                        <NavigationMenuTrigger className={styles.NavigationMenuTrigger}>Кто использует ADA-Parser</NavigationMenuTrigger>
+                                        <NavigationMenuContent className={styles.NavigationMenuContent}>
+                                            {adaUse.map((item) => (
+                                                <Link to="#" key={item.subtitle}>
+                                                    <div className={styles.Header__link}>
+                                                        <img className={styles.Header__link_img} src={item.img} alt="" />
+                                                        <NavigationMenuLink className={styles.Header__link_subtitle}>{item.subtitle}</NavigationMenuLink>
+                                                    </div>
+                                                </Link>
+                                            ))}
+                                        </NavigationMenuContent>
+                                    </NavigationMenuItem>
+                                    <NavigationMenuItem>
+                                        <NavigationMenuTrigger className={`${styles.NavigationMenuTrigger} ${styles.resurs}`}>Ресурсы</NavigationMenuTrigger>
+                                        <NavigationMenuContent className={styles.NavigationMenuContent2}>
+                                            {resources.map((item) => (
+                                                <div key={item.subtitle} className={styles.Header__link}>
+                                                    <img className={styles.Header__link_img} src={item.img} alt="" />
+                                                    <NavigationMenuLink className={styles.Header__link_subtitle}>{item.subtitle}</NavigationMenuLink>
+                                                    <p>{item.text}</p>
+                                                </div>
+                                            ))}
+                                        </NavigationMenuContent>
+                                    </NavigationMenuItem>
+                                </NavigationMenuList>
+                                <Link to="/parsings-list"><h2 className={`${styles.NavigationMenuTrigge} ${styles.NavigationMenuTrigg}`}>Парсеры</h2></Link>
+                                <Link to="/servicesPage"><h2 className={styles.NavigationMenuTrigge}>Услуги</h2></Link>
+                                <Link to="#"><h2 className={styles.NavigationMenuTrigge}>Форум</h2></Link>
+                            </NavigationMenu>
+                            <Link to="#"><button className={styles.BurgerMenu_button}>Купить ADA-Parser</button></Link>
+                        </SheetDescription>
+                    </SheetHeader>
+                </SheetContent>
             </Sheet>
-            
         </div>
     )
 }
