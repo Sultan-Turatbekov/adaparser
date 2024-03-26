31c72e014a15d04b50e02042f58a9e60f996e533
UchihaAdik <adilvar2001@gmail.com>
2024-03-26 18:58:04 +0600

    локально

--- src/components/ServicesPage/Services.tsx
+++ src/components/ServicesPage/Services.tsx
@@ -1,8 +1,17 @@
-
+import styles from "./Services.module.scss"
 
 function Services() {
   return (
-    <div>Services</div>
+    <div className={styles.services}>
+      <div>
+          <h3 className={styles.services_title}>Парсинг данных по вашему техзаданию</h3>
+          <p className={styles.services_text}>Нужно собрать базу телефонов, емейлов, спарсить товары интернет-магазина, получить информацию с досок объявлений? Мы все сделаем за вас, просто оставьте заявку</p>
+          <button className={styles.services_btn}>Заявка на парсинг</button>
+      </div>
+      <div>
+          <img src="" alt="services" />
+      </div>
+    </div>
   )
 }
 
