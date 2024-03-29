65ef140a18d0a53ced8b41cd23eccd9506373e2f
Sula <turatbekovchannel2018@gmail.com>
2024-03-29 22:08:43 +0600

    make Pricing.tsx

--- src/common/Home/Home.tsx
+++ src/common/Home/Home.tsx
@@ -1,12 +1,14 @@
 import {AllParsings} from "@/src/components/shared/AllParsings/AllParsing.tsx";
 import AllEnterLink from "../../components/shared/AllEnterLink/AllEnterLink"
 import homeDataJson from "./homeDataJson.json"
+import {Pricing} from "@/src/components/shared/Pricing/Pricing.tsx";
 
 export const Home = () => {
     return(
         <>
             <AllEnterLink dataJson={homeDataJson}/>
             <AllParsings/>
+            <Pricing/>
         </>
     )
 }
