9d2f2d8b3b8da1fccfed910eeab3848d0b526128
UchihaAdik <adilvar2001@gmail.com>
2024-03-30 17:58:19 +0600

    onlineStore

--- src/common/Home/Home.tsx
+++ src/common/Home/Home.tsx
@@ -1,12 +1,14 @@
 import {AllParsings} from "@/src/components/shared/AllParsings/AllParsing.tsx";
 import AllEnterLink from "../../components/shared/AllEnterLink/AllEnterLink"
 import homeDataJson from "./homeDataJson.json"
+import TopStore from "@/src/components/shared/TopStore/TopStore";
 
 export const Home = () => {
     return(
         <>
             <AllEnterLink dataJson={homeDataJson}/>
             <AllParsings/>
+            <TopStore/>
         </>
     )
 }
